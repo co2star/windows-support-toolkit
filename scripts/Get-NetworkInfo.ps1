75111cc46c936f0816f9d5955bd8609cbe470563
Bahadir Talha Yalaniz <zeroisworld@gmail.com>
2026-09-04 21:27:00 +0200

    Fix network information output formatting

--- scripts/Get-NetworkInfo.ps1
+++ scripts/Get-NetworkInfo.ps1
@@ -1,10 +1,12 @@
 $configs = Get-NetIPConfiguration | Where-Object { $_.NetAdapter.Status -eq 'Up' }
 
-foreach ($config in $configs) {
+$results = foreach ($config in $configs) {
     [PSCustomObject]@{
         Interface      = $config.InterfaceAlias
         IPv4Address    = ($config.IPv4Address.IPAddress -join ', ')
         DefaultGateway = ($config.IPv4DefaultGateway.NextHop -join ', ')
         DnsServers     = ($config.DNSServer.ServerAddresses -join ', ')
     }
-} | Format-Table -AutoSize
+}
+
+$results | Format-Table -AutoSize
